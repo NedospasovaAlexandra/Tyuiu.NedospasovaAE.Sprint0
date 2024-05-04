b365bdc0ecb48c4c9a235d76a049a3f4027e2636
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-04 18:39:10 +0500

    Task4

---
+++ Tyuiu.NedospasovaAE.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NedospasovaAE.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NedospasovaAE.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("276b25d4-232d-4b57-90ef-d58d5918934a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NedospasovaAE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NedospasovaAE.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(6, 2));
+            Console.WriteLine(DataService.Multiplication(2, 3));
+            Console.WriteLine(DataService.Division(6, 3));
+            Console.ReadKey();
+        }
+    }
+}

864f995474524ae103519d6a8f8a7583b1f51cb4
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-04 20:24:15 +0500

    Task6

---
+++ Tyuiu.NedospasovaAE.Sprint0.Task6.V0.Test.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.NedospasovaAE.Sprint0.Task6.V0.Test.Lib
+{
+    public class DataService
+    {
+    }
+}
